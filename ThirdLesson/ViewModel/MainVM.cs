1c94ab1a29c2c89d4d8d5a007f67075894855469
Rasim <allen.wallker.00@bk.ru>
2021-01-28 21:19:46 +0400

    First Form

---
+++ ThirdLesson/ViewModel/MainVM.cs
@@ -0,0 +1,42 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ThirdLesson.Command;
+
+namespace ThirdLesson.ViewModel
+{
+    class MainVM : ViewModelBase
+    {
+          private ViewModelBase currentViewModel;
+         
+          public Commands FirstButton { get; set; }
+          public Commands SecondButton { get; set; }
+         
+          public ViewModelBase CurrentViewModel { get => currentViewModel; set { Set(ref currentViewModel, value); } }
+         
+         public MainVM()
+         {
+              FirstButton = new Commands(x =>First());
+              SecondButton = new Commands(x => Second());
+         }
+         public void First()   
+         {
+             CurrentViewModel = new FirstVM();
+         }
+         public void Second()
+         {
+             CurrentViewModel = new SecondVM();
+         }
+    }
+
+
+
+}
+    
+
---
+++ ThirdLesson/ViewModel/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdLesson.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Set<T>(ref T prop,T value,[CallerMemberName] string name ="")
+        {
+            prop = value;
+            PropertyChanged.Invoke(this,new PropertyChangedEventArgs(name) );
+        }
+    }
+}
